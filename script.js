fb097181472c02d6c17e5c0453cf4a70997e8a13
jennielai <jennie.t.lai001@gmail.com>
2021-11-01 02:52:01 -0400

    wip: fixed music

--- script.js
+++ script.js
@@ -17,14 +17,14 @@ function buildVars(){
   // all the music for the game
   // http://downloads.khinsider.com/game-soundtracks/album/pokemon-gameboy-sound-collection
   music = {
-    opening: "http://66.90.91.26/ost/pokemon-gameboy-sound-collection/aipycrsoym/101-opening.mp3",
-    battle: "http://66.90.91.26/ost/pokemon-gameboy-sound-collection/fllwdebjsg/107-battle-vs-wild-pokemon-.mp3",
-    victory: "http://66.90.91.26/ost/pokemon-gameboy-sound-collection/csqodhnibz/108-victory-vs-wild-pokemon-.mp3",
-    pikachu: "http://66.90.91.26/ost/pokemon-gameboy-sound-collection/hpjacpzwof/170-pikachu.mp3",
-    charmander: "http://66.90.91.26/ost/pokemon-gameboy-sound-collection/wfwdwleyga/149-charmander.mp3",
-    squirtle: "http://66.90.91.26/ost/pokemon-gameboy-sound-collection/soagplijvq/152-squirtle.mp3",
-    bulbasaur: "http://66.90.91.26/ost/pokemon-gameboy-sound-collection/gvqmhhryki/146-bulbasaur.mp3",
-    machop: "http://66.90.91.26/ost/pokemon-gameboy-sound-collection/viaskmobgb/213-machop.mp3"
+    opening: "./music/101-opening.mp3",
+    battle: "./music/107-battle (vs wild pokemon).mp3",
+    victory: "./music/108-victory (vs wild pokemon) (1).mp3",
+    pikachu: "./music/170-pikachu.mp3",
+    charmander: "./music/149-charmander.mp3",
+    squirtle: "./music/152-squirtle.mp3",
+    bulbasaur: "./music/146-bulbasaur.mp3",
+    machop: "./music/213-machop.mp3"
   }
 
   typeSprite = 'http://orig15.deviantart.net/24d8/f/2011/057/3/5/ge___energy_type_icons_by_aschefield101-d3agp02.png';
